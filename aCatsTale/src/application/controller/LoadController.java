4d210a89fd8394db8e6357c03700c66c5ab500fd
Isabella <66403691+isabella-talijancic@users.noreply.github.com>
2022-11-03 12:50:28 -0500

    added a load page between selector view and game view, animated text for team member names, changes order of scenes to make it simpler for user, edits to how scenes look like, etc.

---
+++ aCatsTale/src/application/controller/LoadController.java
@@ -0,0 +1,101 @@
+package application.controller;
+
+import javafx.animation.Animation;
+import javafx.animation.Transition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
+
+import application.Main;
+import application.model.Tamagotchi;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
+/**
+ * MainController is a Java class that will need to implement the EventHandler interface,
+ * and handle any events that occur when the user interacts with Main.fxml. 
+ * 
+ * @author Isabella Talijancic (juu530)
+ * UTSA CS 3443 - A Cat's Tale
+ * Fall 2022
+ */
+public class LoadController implements EventHandler<ActionEvent>, Initializable {
+	
+	@FXML
+	Button gameStartButton;
+	
+	@FXML
+    ImageView bannerImageView;
+	
+	@FXML
+    Image bannerImage;
+	
+	@FXML
+	MediaPlayer mediaPlayer, mediaPlayerSFX;
+	
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    	//backgroundMusic();
+
+    }
+    
+	@Override
+	public void handle(ActionEvent event) {
+		
+		Image bannerImage = new Image("images/loadgif.gif");
+		bannerImageView.setImage(bannerImage);
+		
+		//check which button was pressed...
+		Button sourceButton = (Button) event.getSource();
+		String buttonText = sourceButton.getText();
+				
+		if(buttonText!=null && buttonText.contains("Game Start!") ) {
+			// move the user back to the main view
+			try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/LivingRoom.fxml"));
+				Main.stage.setScene( new Scene(root, 600, 600) );
+				Main.stage.show();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			//else
+		}
+	}
+	
+	//Event listener to play a sound effect for when a user hovers over a button.
+    public void buttonHovered() {
+        playSound("cuteclick");
+    }
+	
+    //Creates a new Media object to play sound effects.
+    public void playSound(String soundName) {
+        String s = "src/application/sounds/" + soundName + ".mp3";
+        Media h = new Media(Paths.get(s).toUri().toString());
+        mediaPlayerSFX = new MediaPlayer(h);
+        mediaPlayerSFX.play();
+    }
+	
+//	public void backgroundMusic() {
+//        String s = "src/application/sounds/nyancat.mp3";
+//        Media h = new Media(Paths.get(s).toUri().toString());
+//        mediaPlayer = new MediaPlayer(h);
+//        mediaPlayer.play();
+//    }
+
+}
